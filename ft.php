fe22ad41523630b7386d48902c8043407e30130f
chrysalides <chrysalides.f1@gmail.com>
2017-01-06 17:34:48 +0900

    Update ft.php

--- ft.php
+++ ft.php
@@ -1,1 +1,2 @@
+<?php require_once( dirname( dirname(dirname( __FILE__ ))) . '/wp-load.php' ); ?>
 <footer><small>Copyright <i class="fa fa-copyright"></i> <?php bloginfo('name'); ?>,<?php echo date("Y"); ?> All Rights Reserved.</small></footer>
